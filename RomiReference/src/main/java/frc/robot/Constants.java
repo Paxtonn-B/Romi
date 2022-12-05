451f90850b42016002cf7bbfe0d779d955befab3
BCR <97000464+Paxtonn-B@users.noreply.github.com>
2022-12-04 16:51:40 -0800

    Update Constant
    keep watching the video Paxton - 14:53 / 48:17

--- RomiReference/src/main/java/frc/robot/Constants.java
+++ RomiReference/src/main/java/frc/robot/Constants.java
@@ -12,4 +12,24 @@ package frc.robot;
  * <p>It is advised to statically import this class (or one of its inner classes) wherever the
  * constants are needed, to reduce verbosity.
  */
-public final class Constants {}
+public final class Constants {
+    public static final class DriveConstants{
+        //Romi has the left and right motors set to 
+        //PWM channels 0 and 1 respectively
+        public static final int leftDriveMotor = 0;
+        public static final int rightDriveMotor = 1;
+
+        //The Romi has onboard encoders that are hardcoded
+        //to use DIO pins 4/5 and 6/7 for the left and right
+        public static final int leftEncoderA = 4;
+        public static final int leftEncoderB = 5;
+        public static final int rightEncoderA = 6;
+        public static final int rightEncoderB = 7;
+
+        public static final double kCountsPerRevolution = 1440.0;
+        public static final double kWheelDiameterInch = 2.75591; // 70mm
+        public static final double kWheelSpacingInch = 5.50;
+
+        public static final double kP_drive = 0.3;
+    }
+}
